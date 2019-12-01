eea51895b14504886a19a1a3c1f52aac1d5583c9
mlutfiibra <mlutfiibra@outlook.com>
2019-12-01 20:40:44 +0700

    Fix 404

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,21 +1,21 @@
-const routes = require('express').Router();
-const products = require('./products')
-const carts = require('./carts')
-const users = require('./users')
-const transactions = require('./transactions')
-const UserController = require('../controllers/UserController')
-const Authentication = require('../middlewares/authenticate')
+const routes = require("express").Router();
+const products = require("./products");
+const carts = require("./carts");
+const users = require("./users");
+const transactions = require("./transactions");
+const UserController = require("../controllers/UserController");
+const Authentication = require("../middlewares/authenticate");
 
-routes.post('/register', UserController.register)
-routes.post('/login', UserController.login)
+routes.post("/register", UserController.register);
+routes.post("/login", UserController.login);
 
-routes.use('/users', users)
-routes.use('/products', products)
-routes.use('/carts', Authentication, carts)
-routes.use('/transactions', Authentication, transactions)
+routes.use("/users", users);
+routes.use("/products", products);
+routes.use("/carts", Authentication, carts);
+routes.use("/transactions", Authentication, transactions);
 
-routes.get('*', (req,res) => {
-    res.status(404).json('Page not found')
-})
+routes.get("*", (req, res) => {
+  res.status(404).json("Cannot find routes");
+});
 
-module.exports = routes
+module.exports = routes;
