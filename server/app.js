bb5723d5d8b1af114c3b69deaea007dd1253d860
mlutfiibra <mlutfiibra@outlook.com>
2019-12-01 20:43:41 +0700

    Implement Unified Topology

--- server/app.js
+++ server/app.js
@@ -1,36 +1,40 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT
-const routes = require('./routes')
-const mongoose = require('mongoose');
-const cors = require('cors')
-const morgan = require('morgan')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const routes = require("./routes");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
 
-mongoose.set('useCreateIndex', true)
+mongoose.set("useCreateIndex", true);
 
-if(process.env.NODE_ENV === 'test') {
-    process.env.MONGODB_CONNECTION = `mongodb://localhost/gamestation-test`
+if (process.env.NODE_ENV === "test") {
+  process.env.MONGODB_CONNECTION = `mongodb://localhost/gamestation-test`;
 }
 
-mongoose.set('useFindAndModify', false);
-mongoose.connect(process.env.MONGODB_CONNECTION, {useNewUrlParser: true})
-.then(function(success){
-    console.log("succesfully connect to database")
-})
-.catch(function(err){
-    console.log(err)
-});
+mongoose.set("useFindAndModify", false);
+mongoose
+  .connect(process.env.MONGODB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(function(success) {
+    console.log("succesfully connect to database");
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
 
-app.use(cors())
-app.use(morgan('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended:false }))
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/', routes)
+app.use("/", routes);
 
 app.listen(PORT, () => {
-    console.log(`Running on http://localhost:${PORT}`)
-})
+  console.log(`Running on http://localhost:${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
